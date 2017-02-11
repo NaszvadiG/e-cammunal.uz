c28fd141d1fcc64d4a3d6c864fc852f98c9776aa
Elyor <elyor@outlook.uz>
2017-02-12 02:57:44 +0500

    clean

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost:81/e-cammunal.uz/';
+$config['base_url'] = 'http://localhost:81/e-cammunal.uz/web/';
 
 /*
 |--------------------------------------------------------------------------
